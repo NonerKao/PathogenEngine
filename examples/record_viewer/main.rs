ab45ab357c7bc01b6c2381d211a5cb2d58156eca
Quey-Liang Kao <s101062801@m101.nthu.edu.tw>
2024-08-25 13:09:57 +0800

    [record_viewer] Implement sgf loading

--- examples/record_viewer/main.rs
+++ examples/record_viewer/main.rs
@@ -1,29 +1,215 @@
 use actix_files as fs;
 use actix_web::{web, App, HttpServer, Responder, Result};
+use clap::Parser;
 use serde_derive::Serialize;
+use std::fs::File;
+use std::io::Read;
+use std::sync::Arc;
+use std::sync::Mutex;
 
-#[derive(Serialize)]
+use pathogen_engine::core::tree::*;
+use pathogen_engine::core::*;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// SGF files to be loaded from
+    #[arg(short, long)]
+    load: String,
+}
+
+#[derive(Serialize, Clone)]
 struct GameState {
     white_positions: Vec<String>,
     black_positions: Vec<String>,
+    steps: Vec<Step>,
+}
+#[derive(Serialize, Clone)]
+struct Step {
+    id: u32,
+    pos: String,
+    is_marker: bool,
+    char1: char,
+    marker: i32,
 }
 
-async fn game_state() -> impl Responder {
-    let state = GameState {
-        white_positions: vec!["db", "ef", "fe"].into_iter().map(String::from).collect(),
-        black_positions: vec!["bd", "af", "fd"].into_iter().map(String::from).collect(),
-    };
-    web::Json(state)
+async fn game_state(data: web::Data<AppState>) -> impl Responder {
+    let gs = data.amgs.lock().unwrap();
+    web::Json(gs.clone())
 }
 
 async fn index() -> Result<fs::NamedFile> {
     Ok(fs::NamedFile::open("static/index.html")?)
 }
 
+struct AppState {
+    amgs: Arc<Mutex<GameState>>,
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
+    // Load the record
+    let args = Args::parse();
+    let mut contents = String::new();
+    print!("Loading ... ");
+    let mut file = File::open(args.load)?;
+    file.read_to_string(&mut contents)
+        .expect("Failed to read file");
+    let mut iter = contents.trim().chars().peekable();
+    let t = TreeNode::new(&mut iter, None);
+    let g = Game::init(Some(t));
+    if !g.is_ended() {
+        panic!("The game is not yet finished");
+    }
+
+    let setup0_h = g.history.borrow().to_root().borrow().children[0]
+        .borrow()
+        .children[0]
+        .clone();
+    let setup0_u = g.history.borrow().to_root().borrow().children[0]
+        .borrow()
+        .children[0]
+        .borrow()
+        .children[0]
+        .clone();
+
+    let mut gs = GameState {
+        white_positions: setup0_h.borrow().properties[1].value.clone(),
+        black_positions: setup0_u.borrow().properties[1].value.clone(),
+        steps: vec![],
+    };
+
+    let mut id = 0;
+    let setup1_0 = setup0_u.borrow().children[0].clone();
+    let setup1_1 = setup1_0.borrow().children[0].clone();
+    let setup1_2 = setup1_1.borrow().children[0].clone();
+    let setup1_3 = setup1_2.borrow().children[0].clone();
+    gs.steps.push(Step {
+        id: id,
+        pos: setup1_0.borrow().properties[1].value[0].clone(),
+        is_marker: true,
+        char1: ' ',
+        marker: -1,
+    });
+    id = id + 1;
+    gs.steps.push(Step {
+        id: id,
+        pos: setup1_1.borrow().properties[1].value[0].clone(),
+        is_marker: true,
+        char1: ' ',
+        marker: -1,
+    });
+    id = id + 1;
+    gs.steps.push(Step {
+        id: id,
+        pos: setup1_2.borrow().properties[1].value[0].clone(),
+        is_marker: true,
+        char1: ' ',
+        marker: -1,
+    });
+    id = id + 1;
+    gs.steps.push(Step {
+        id: id,
+        pos: setup1_3.borrow().properties[1].value[0].clone(),
+        is_marker: true,
+        char1: ' ',
+        marker: -1,
+    });
+    id = id + 1;
+
+    let setup2_0 = setup1_3.borrow().children[0].clone();
+    let setup2_1 = setup2_0.borrow().children[0].clone();
+    let setup2_2 = setup2_1.borrow().children[0].clone();
+    let setup2_3 = setup2_2.borrow().children[0].clone();
+    gs.steps.push(Step {
+        id: id,
+        pos: setup2_0.borrow().properties[1].value[0].clone(),
+        is_marker: false,
+        char1: 'D',
+        marker: 0,
+    });
+    id = id + 1;
+    gs.steps.push(Step {
+        id: id,
+        pos: setup2_1.borrow().properties[1].value[0].clone(),
+        is_marker: false,
+        char1: 'P',
+        marker: 0,
+    });
+    id = id + 1;
+    gs.steps.push(Step {
+        id: id,
+        pos: setup2_2.borrow().properties[1].value[0].clone(),
+        is_marker: false,
+        char1: 'D',
+        marker: 0,
+    });
+    id = id + 1;
+    gs.steps.push(Step {
+        id: id,
+        pos: setup2_3.borrow().properties[1].value[0].clone(),
+        is_marker: false,
+        char1: 'P',
+        marker: 0,
+    });
+    id = id + 1;
+
+    let setup3_0 = setup2_3.borrow().children[0].clone();
+    gs.steps.push(Step {
+        id: id,
+        pos: setup3_0.borrow().properties[1].value[0].clone(),
+        is_marker: false,
+        char1: 'D',
+        marker: 0,
+    });
+    id = id + 1;
+
+    let mut curr_node = setup3_0.borrow().children[0].clone();
+    loop {
+        // opening
+        let side = if curr_node.borrow().properties[0].ident == "W" {
+            'D'
+        } else {
+            'P'
+        };
+
+        // core
+        let mut route = vec![];
+        for s in curr_node.borrow().properties[0].value.iter() {
+            let is_marker = route.contains(s);
+            gs.steps.push(Step {
+                id: id,
+                pos: s.clone(),
+                is_marker: is_marker,
+                char1: side,
+                marker: if !is_marker { 0 } else if side == 'D' { 1 } else { -1 },
+            });
+
+            id = id + 1;
+            if !is_marker {
+                route.push(s.clone());
+            }
+        }
+
+        // closing
+        if curr_node.borrow().children.len() == 0 {
+            break;
+        } else {
+            let temp = curr_node.borrow().children[0].clone();
+            curr_node = temp;
+        }
+    }
+
+    println!("... Done");
+
+    let setup_state = web::Data::new(AppState {
+        amgs: Arc::new(Mutex::new(gs)),
+    });
+
+    // Start the service
+    HttpServer::new(move || {
         App::new()
+            .app_data(setup_state.clone())
             .route("/", web::get().to(index))
             .route("/game_state", web::get().to(game_state))
             .service(fs::Files::new("/static", "static").show_files_listing())
@@ -32,4 +218,3 @@ async fn main() -> std::io::Result<()> {
     .run()
     .await
 }
-
--- examples/record_viewer/static/app.js
+++ examples/record_viewer/static/app.js
@@ -24,19 +24,8 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     }
 
-    // Function to generate a random color
-    function getRandomColor() {
-        const letters = '0123456789ABCDEF';
-        let color = '#';
-        for (let i = 0; i < 6; i++) {
-            color += letters[Math.floor(Math.random() * 16)];
-        }
-        return color;
-    }
-
     // Handle click on the 6x6 grid
     function handleClick6x6(cell) {
-        cell.style.backgroundColor = getRandomColor();
         console.log(`6x6 Grid clicked at (${cell.dataset.row}, ${cell.dataset.col})`);
     }
 
@@ -58,43 +47,150 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 
     // Handle scroll events
-    function handleScroll(event) {
-        if (event.deltaY < 0) {
-            console.log("Scrolled up");
-        } else {
-            console.log("Scrolled down");
+    function handleScroll(direction) {
+        clearGridState();
+
+        if (direction === "up" && current_id < steps.length - 1) {
+            applyStepForward(steps[current_id], steps[current_id+1]);
+            current_id++;
+        } else if (direction === "down" && current_id > -1) {
+            applyStepBackward(steps[current_id], steps[current_id-1]);
+            current_id--;
         }
+
+        drawGrid();
+    }
+
+    const gridState = Array.from({ length: 6 }, () => Array(6).fill(null));
+    let current_id = -1; // Start before the first step
+    steps = [];
+
+    // Function to clear the grid state
+    function clearGridState() {
+        gridState.forEach((row, rowIndex) => {
+            row.forEach((_, colIndex) => {
+                gridState[rowIndex][colIndex] = null;
+            });
+        });
+    }
+
+    // Function to draw the grid based on the current gridState
+    function drawGrid() {
+        gridState.forEach((row, rowIndex) => {
+            row.forEach((cellState, colIndex) => {
+                const cell = document.querySelector(`#grid-6x6 div[data-row="${rowIndex}"][data-col="${colIndex}"]`);
+                if (cellState) {
+                    const char1Class = token.char1 === "D" ? "token-blue" : 
+                                       token.char1 === "P" ? "token-red" : "";
+                    const char2Class = token.color === "blue" ? "token-blue" : 
+                                       token.color === "red" ? "token-red" : "";
+
+                    cell.innerHTML = `<span class="${char1Class}">${cellState.char1}</span><span class="${char2Class}">${cellState.char2}</span>`;
+                } else {
+                    cell.innerHTML = ''; // Clear the cell if there's no state
+                }
+            });
+        });
+    }
+
+    function applyStepForward(from, to) {
+        const (from_on_map, [fcol, frow]) = convertPosition(from.pos);
+        const (to_on_map, [tcol, trow]) = convertPosition(to.pos);
+        if (from.char1 != to.char1) {
+            // switch player
+            // XXX: Remove previous map position, ...
+            // XXX: Display `to`
+        } else if (from.is_marker != to.is_marker) {
+            // from BoardMove to SetMarker, nothing to be done
+        } else {
+            if (from_on_map != to_on_map) {
+                // from SetMap/Lockdown to SetCharacter, nothing to be done
+            } else if (from_on_map) {
+                // Lockdown
+                // XXX: Remove previous plague's map position
+                // XXX: Display `to`
+            } else {
+                if (from.is_marker) {
+                    // SetMarker
+                    // XXX: add `to.marker`
+                } else {
+                    // BoardMove
+                    // XXX: Remove `from.char1`
+                    // XXX: Display `to`
+                }
+            }
+        }
+        //gridState[row][col] = { char1: step.char1, char2: step.char2, color: step.color };
+    }
+
+    function applyStepBackward(step) {
+        const (on_map, [col, row]) = convertPosition(step.pos);
+        gridState[row][col] = { char1: step.char1, char2: step.char2, color: step.color };
     }
 
     // Apply throttle to the scroll event
-    document.addEventListener("wheel", throttle(handleScroll, 150));
+    document.addEventListener("wheel", throttle((event) => {
+        if (event.deltaY < 0) {
+            handleScroll("up");
+        } else {
+            handleScroll("down");
+        }
+    }, 150));
 
     fetch('/game_state')
         .then(response => response.json())
         .then(data => {
             setupGrid(data.white_positions, data.black_positions);
-        });
+            steps = data.steps;
+        })
 
     function setupGrid(whitePositions, blackPositions) {
+        // Setup0
         whitePositions.forEach(pos => {
-            const [col, row] = convertPosition(pos);
+            const (_, [col, row]) = convertPosition(pos);
             const cell = document.querySelector(`#grid-6x6 div[data-row="${row}"][data-col="${col}"]`);
             if (cell) cell.style.backgroundColor = 'white';
         });
 
         blackPositions.forEach(pos => {
-            const [col, row] = convertPosition(pos);
+            const (_, [col, row]) = convertPosition(pos);
             const cell = document.querySelector(`#grid-6x6 div[data-row="${row}"][data-col="${col}"]`);
             if (cell) cell.style.backgroundColor = 'black';
         });
     }
 
+    // Return (on_map, [col, row])
     function convertPosition(pos) {
         // Convert SGF-style coordinates to grid indices
-        const col = pos.charCodeAt(0) - 'a'.charCodeAt(0);
-        const row = pos.charCodeAt(1) - 'a'.charCodeAt(0);
-        return [col, row];
+        let col = pos.charCodeAt(0) - 'a'.charCodeAt(0);
+        let row = pos.charCodeAt(1) - 'a'.charCodeAt(0);
+        if col > 5 {
+	    return (true, [col - 8, row - 8]); 
+        } else {
+	    return (false, [col, row]); 
+        }
     }
+
+    // Function to apply tokens to the grid
+    function applyTokens(tokens) {
+        tokens.forEach(token => {
+            const [row, col] = token.pos.split("").map(Number);
+            const cell = document.querySelector(`#grid-6x6 div[data-row="${row}"][data-col="${col}"]`);
+            if (cell) {
+                bg_white = cell.style.backgroundColor === 'white';
+	        console.log(` (${cell.dataset.row}, ${cell.dataset.col}): (${cell.style.backgroundColor})`);
+                const char1 = token.char1 === "D" ? "token-blue" : 
+                              token.char1 === "P" ? "token-red" : "";
+                const char2 = token.color === "blue" ? "token-blue" : 
+                              token.color === "red" ? "token-red" : "";
+
+                cell.innerHTML = `<span class="${char1}">${token.char1}</span><span class="${char2}">${token.char2}</span>`;
+            }
+        });
+    }
+
+    // Apply tokens (use your game state instead of the hardcoded tokens)
+    applyTokens(tokens);
 });
 
 
